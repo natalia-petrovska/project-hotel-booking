6ab88e4071ed7e53765190d8840ab53694ebc0b5
Natalia Petrovska <natalia.petrovska87@gmail.com>
2025-05-20 16:17:50 +0200

    added new function

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ function selectRoom(roomName) {
 }
 
 function submitForm(event) {
+
   event.preventDefault();
 
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const room = document.getElementById("room").value;
+  const checkin = new Date(document.getElementById("checkin").value);
+  const checkout = new Date(document.getElementById("checkout").value);
+
+  if (checkin >= checkout) {
+    alert("❗ Check-out date must be after Check-in date.");
+    return;
+  }
 
   if (name && email && room) {
     document.getElementById("confirmation").classList.remove("hidden");
   }
+
 }
-
-
